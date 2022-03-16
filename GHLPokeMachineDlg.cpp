0c481cd44bac316115828230dfa41e6d140fa205
Octave13 <gabriel.ouaknine-beaulieu.1@ens.etsmtl.ca>
2022-03-16 09:36:55 -0400

    Puts Icons to the System Tray (Not working yet)

--- GHLPokeMachineDlg.cpp
+++ GHLPokeMachineDlg.cpp
@@ -101,17 +101,56 @@ BOOL CGHLPokeMachineDlg::OnInitDialog()
 	SetIcon(m_hIcon, TRUE);			// Définir une grande icône
 	SetIcon(m_hIcon, FALSE);		// Définir une petite icône
 
+	
+									
+
 	// TODO: ajoutez ici une initialisation supplémentaire
+	// Declare NOTIFYICONDATA details. 
+	// 
+	// Error handling is omitted here for brevity. Do not omit it in your code.
+
+	NOTIFYICONDATA nid = {};
+	nid.cbSize = sizeof(nid);
+	nid.hWnd = hWnd;
+	nid.uFlags = NIF_ICON | NIF_TIP | NIF_GUID;
+
+	// Note: This is an example GUID only and should not be used.
+	// Normally, you should use a GUID-generating tool to provide the value to
+	// assign to guidItem.
+	static const GUID myGUID =
+	{ 0x23977b55, 0x10e0, 0x4041, {0xb8, 0x62, 0xb1, 0x95, 0x41, 0x96, 0x36, 0x69} };
+	nid.guidItem = myGUID;
+
+	// This text will be shown as the icon's tooltip.
+	StringCchCopy(nid.szTip, ARRAYSIZE(nid.szTip), L"Test application");
+
+	// Load the icon for high DPI.
+	LoadIconMetric(hInst, MAKEINTRESOURCE(IDI_SMALL), LIM_SMALL, &(nid.hIcon));
+
+	// Show the notification.
+	Shell_NotifyIcon(NIM_ADD, &nid) ? S_OK : E_FAIL;
+
 
 	return TRUE;  // retourne TRUE, sauf si vous avez défini le focus sur un contrôle
 }
 
+/* Lorsqu'on appuis sur qqch?*/
 void CGHLPokeMachineDlg::OnSysCommand(UINT nID, LPARAM lParam)
 {
 	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
 	{
 		CAboutDlg dlgAbout;
 		dlgAbout.DoModal();
+	}
+	else if ((nID & 0xFFF0) == 0xF120)
+	{
+		/*Maximise*/
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+	else if ((nID & 0xFFF0) == 0xF020)
+	{
+		/*Minimize*/
+		CDialogEx::OnSysCommand(nID, lParam);
 	}
 	else
 	{
@@ -144,6 +183,7 @@ void CGHLPokeMachineDlg::OnPaint()
 	}
 	else
 	{
+		/* Passer de minimize a grand*/
 		CDialogEx::OnPaint();
 	}
 }
@@ -173,7 +213,7 @@ void CGHLPokeMachineDlg::OnBnClickedCancel()
 {
 	StopGHPoke(hThreadArray, pDeviceData);
 	// TODO: ajoutez ici le code de votre gestionnaire de notification de contrôle
+
 	CDialogEx::OnCancel();
 }
-
 
