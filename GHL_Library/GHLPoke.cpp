1be5e9d0dec4029224dc05922717ee21e3d6b856
Octave13 <gabriel.ouaknine-beaulieu.1@ens.etsmtl.ca>
2022-03-23 00:56:12 -0400

    Initialize Data

--- GHL_Library/GHLPoke.cpp
+++ GHL_Library/GHLPoke.cpp
@@ -10,6 +10,7 @@ const CHAR Ps3WiiuPokeMessage[POKE_MESSAGE_LENGTH] = PS3_WIIU_POKE_MESSAGE;
 const CHAR Ps4PokeMessage[POKE_MESSAGE_LENGTH] = PS4_POKE_MESSAGE;
 
 DeviceType CheckVidPid(WCHAR* DevicePath);
+BOOL InitializeData(PDEVICE_DATA pDeviceData, WCHAR* DevicePath, DeviceType Device);
 DWORD WINAPI SendPokeMessage(LPVOID lpParam);
 
 HRESULT StartGHPoke(_Out_opt_ PBOOL  FailureDeviceNotFound, DWORD   dwThreadIdArray[], HANDLE  hThreadArray[], PDEVICE_DATA pDeviceData[])
@@ -29,79 +30,58 @@ HRESULT StartGHPoke(_Out_opt_ PBOOL  FailureDeviceNotFound, DWORD   dwThreadIdAr
         *FailureDeviceNotFound = FALSE;
     }
 
-    //
-    // Enumerate all devices exposing the interface
-    //
+    // Enumerate all devices exposing the HID interface
     deviceInfo = SetupDiGetClassDevs(&GUID_DEVINTERFACE_HID, NULL, NULL, DIGCF_PRESENT | DIGCF_DEVICEINTERFACE);
-
-    if (deviceInfo == INVALID_HANDLE_VALUE) {
-
+    if (deviceInfo == INVALID_HANDLE_VALUE) 
+    {
         hr = HRESULT_FROM_WIN32(GetLastError());
         return hr;
     }
-
     interfaceData.cbSize = sizeof(SP_DEVICE_INTERFACE_DATA);
+
 
     while (Continue)
     {
-        //
         // Get the next interface (index i) in the result set
-        //
         bResult = SetupDiEnumDeviceInterfaces(deviceInfo, NULL, &GUID_DEVINTERFACE_HID, i, &interfaceData);
-
         if (FALSE == bResult)
         {
-
-            //
             // We would see this error if no devices were found
-            //
             if (ERROR_NO_MORE_ITEMS == GetLastError() && NULL != FailureDeviceNotFound)
             {
-
                 if (j == 0)
                 {
                     *FailureDeviceNotFound = TRUE;
                 }
             }
-
             hr = HRESULT_FROM_WIN32(GetLastError());
             SetupDiDestroyDeviceInfoList(deviceInfo);
             return hr;
         }
 
-        //
         // Get the size of the path string
         // We expect to get a failure with insufficient buffer
-        //
         bResult = SetupDiGetDeviceInterfaceDetail(deviceInfo, &interfaceData, NULL, 0, &requiredLength, NULL);
-
         if (FALSE == bResult && ERROR_INSUFFICIENT_BUFFER != GetLastError())
         {
-
             hr = HRESULT_FROM_WIN32(GetLastError());
             SetupDiDestroyDeviceInfoList(deviceInfo);
             return hr;
         }
-        //
-        // Allocate temporary space for SetupDi structure
-        //
-        detailData = (PSP_DEVICE_INTERFACE_DETAIL_DATA)LocalAlloc(LMEM_FIXED, requiredLength);
 
+        // Allocate temporary space for SetupDi structure
+        detailData = (PSP_DEVICE_INTERFACE_DETAIL_DATA)LocalAlloc(LMEM_FIXED, requiredLength);
         if (NULL == detailData)
         {
             hr = E_OUTOFMEMORY;
             SetupDiDestroyDeviceInfoList(deviceInfo);
             return hr;
         }
-
         detailData->cbSize = sizeof(SP_DEVICE_INTERFACE_DETAIL_DATA);
         length = requiredLength;
 
-        //
         // Get the interface's path string
-        //
         bResult = SetupDiGetDeviceInterfaceDetail(deviceInfo, &interfaceData, detailData, length, &requiredLength, NULL);
-
         if (FALSE == bResult)
         {
             hr = HRESULT_FROM_WIN32(GetLastError());
@@ -110,6 +90,7 @@ HRESULT StartGHPoke(_Out_opt_ PBOOL  FailureDeviceNotFound, DWORD   dwThreadIdAr
             return hr;
         }
 
+        /* Check if The device is one from the list */
         if (Device = CheckVidPid(detailData->DevicePath))
         {
             /* Memory Allocation for Data */
@@ -122,36 +103,33 @@ HRESULT StartGHPoke(_Out_opt_ PBOOL  FailureDeviceNotFound, DWORD   dwThreadIdAr
                 return hr;
             }
 
-            /* Copy Device Path in device Data */
-            StringCbCopy(pDeviceData[j]->DevicePath, MAX_PATH - 1, detailData->DevicePath);
-
-            memcpy_s(pDeviceData[j]->PokeMessage, POKE_MESSAGE_LENGTH, Ps4PokeMessage, POKE_MESSAGE_LENGTH);
-
-            hThreadArray[j] = CreateThread(
-                NULL,                   // default security attributes
-                0,                      // use default stack size  
-                SendPokeMessage,       // thread function name
-                pDeviceData[j], // argument to thread function 
-                0,                      // use default creation flags 
-                &dwThreadIdArray[j]);   // returns the thread identifier 
-
-            if (hThreadArray[j] == NULL)
+            /* Initialize Data */
+            if (InitializeData(pDeviceData[j], detailData->DevicePath, Device))
             {
-                LocalFree(detailData);
-                SetupDiDestroyDeviceInfoList(deviceInfo);
-                ExitProcess(3);
+                hThreadArray[j] = CreateThread(
+                    NULL,                   // default security attributes
+                    0,                      // use default stack size  
+                    SendPokeMessage,       // thread function name
+                    pDeviceData[j], // argument to thread function 
+                    0,                      // use default creation flags 
+                    &dwThreadIdArray[j]);   // returns the thread identifier 
+                if (hThreadArray[j] == NULL)
+                {
+                    LocalFree(detailData);
+                    SetupDiDestroyDeviceInfoList(deviceInfo);
+                    ExitProcess(3);
+                }
+                j++;
             }
-
-            j++;
-
         }
         else
         {
             LocalFree(detailData);
         }
+
+        /* Increment Device Interface Index*/
         i++;
     }
-
 
     LocalFree(detailData);
     SetupDiDestroyDeviceInfoList(deviceInfo);
@@ -170,19 +148,56 @@ DeviceType CheckVidPid(WCHAR *DevicePath)
         return UNKNOWN_DEVICE;
 }
 
+
+BOOL InitializeData(PDEVICE_DATA pDeviceData, WCHAR* DevicePath, DeviceType Device)
+{
+    /* Copy Device Path in device Data */
+    if (StringCbCopy(pDeviceData->DevicePath, MAX_PATH - 1, DevicePath) != S_OK)
+    {
+        return FALSE;
+    }
+    /* Open File */
+    pDeviceData->DeviceHandle = CreateFile(pDeviceData->DevicePath, GENERIC_WRITE | GENERIC_READ, FILE_SHARE_WRITE | FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_FLAG_OVERLAPPED, NULL);
+    if (INVALID_HANDLE_VALUE == pDeviceData->DeviceHandle)
+    {
+        return FALSE;
+    }
+    /* Flag for handle open */
+    pDeviceData->HandlesOpen = TRUE;
+
+    /* Initialize Specific Device Data */
+    switch (Device)
+    {
+    case PS4: 
+        memcpy_s(pDeviceData->PokeMessage, POKE_MESSAGE_LENGTH, Ps4PokeMessage, POKE_MESSAGE_LENGTH);
+        pDeviceData->SleepTime = PS4_SLEEP_TIME;
+        break;
+    case PS3_WIIU:
+        memcpy_s(pDeviceData->PokeMessage, POKE_MESSAGE_LENGTH, Ps3WiiuPokeMessage, POKE_MESSAGE_LENGTH);
+        pDeviceData->SleepTime = PS3_WIIU_SLEEP_TIME;
+        break;
+    default: 
+        break;
+    }
+
+    /* Send a first Poke. Some device have more than one interface */
+    if (!HidD_SetOutputReport(pDeviceData->DeviceHandle, pDeviceData->PokeMessage, POKE_MESSAGE_LENGTH * sizeof(CHAR)))
+    {
+        WinUsb_Free(pDeviceData->WinusbHandle);
+        CloseHandle(pDeviceData->DeviceHandle);
+        pDeviceData->HandlesOpen = FALSE;
+        return FALSE;
+    }
+
+    return TRUE;
+
+}
+
 DWORD WINAPI SendPokeMessage(LPVOID lpParam)
 {
     HRESULT hr = S_OK;
     PDEVICE_DATA DeviceData = (PDEVICE_DATA)lpParam;
     BOOL    bResult;
-
-    DeviceData->DeviceHandle = CreateFile(DeviceData->DevicePath, GENERIC_WRITE | GENERIC_READ, FILE_SHARE_WRITE | FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_FLAG_OVERLAPPED, NULL);
-
-    if (INVALID_HANDLE_VALUE == DeviceData->DeviceHandle)
-    {
-        hr = HRESULT_FROM_WIN32(GetLastError());
-        return hr;
-    }
 
     while (Continue)
     {
